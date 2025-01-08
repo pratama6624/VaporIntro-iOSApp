9d61d32d85b0139c63c89f152f76e979719a7bb2
pratama6624 <scyoung6624@gmail.com>
2025-01-08 07:35:14 +0700

    Initial Commit

---
+++ VaporIntro-iOSApp/VaporIntro_iOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VaporIntro_iOSAppApp.swift
+//  VaporIntro-iOSApp
+//
+//  Created by Pratama One on 08/01/25.
+//
+
+import SwiftUI
+
+@main
+struct VaporIntro_iOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
